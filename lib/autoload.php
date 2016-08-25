bd0cc379fe66a28c2f0c4bff693ae3d3a140712f
Matt Boynes <mboynes@alleyinteractive.com>
2016-08-24 20:07:18 -0400

    Initial commit

---
+++ lib/autoload.php
@@ -0,0 +1,21 @@
+<?php
+namespace ES_Admin;
+
+/**
+ * Autoload classes.
+ *
+ * @param  string $cls Class name.
+ */
+function autoload( $cls ) {
+	$cls = ltrim( $cls, '\\' );
+	if ( strpos( $cls, 'ES_Admin\\' ) !== 0 ) {
+		return;
+	}
+
+	$cls = strtolower( str_replace( [ 'ES_Admin\\', '_' ], [ '', '-' ], $cls ) );
+	$dirs = explode( '\\', $cls );
+	$cls = array_pop( $dirs );
+
+	require_once( PATH . rtrim( '/lib/' . implode( '/', $dirs ), '/' ) . '/class-' . $cls . '.php' );
+}
+spl_autoload_register( '\ES_Admin\autoload' );
